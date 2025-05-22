f42ac0d0cc950dbff359a49af991f80c1bbfc6d8
pla1n <danilkuisis@gmail.com>
2025-05-22 19:39:46 +0300

    d

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,30 @@
-export const App = () => {
-  return (
-    <div>
-      
-    </div>
-  );
+import { Component } from "react";
+import { Modal } from "./Modal/Modal";
+
+export class App extends Component {
+  state = {
+    showModal: false,
+  }
+
+  toggleModal = () => {
+    this.setState(({showModal}) => ({
+      showModal: !showModal,
+    }))
+  }
+
+  render() {
+    const {showModal} = this.state
+    return (
+      <div>
+        <button type='button' onClick={this.toggleModal}>Open Modal</button>
+
+        {showModal && <Modal onModalClose={this.toggleModal}>
+          <button onClick={this.toggleModal} type="button">X</button>
+          <h1>Title</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, ut.</p>
+        </Modal>}
+        
+      </div>
+    );
+  }
 };
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,5 +1,33 @@
-import { Component } from "react";
+import { Backdrop, ModalWindow } from './Modal.styled'
+import React, { Component } from 'react';
 
 export class Modal extends Component {
     
+    handleClick = (e) => {
+        if (e.code === 'Escape') {
+            this.props.onModalClose();
+        }
+    }
+
+    componentDidMount() {
+        window.addEventListener('keydown', this.handleClick)
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('keydown', this.handleClick)
+    }
+
+    handleBackdropClick = (e) => {
+        if (e.targer === e.currentTarget) {
+            this.props.onModalClose()
+        }        
+    }
+
+    render() {
+        return (
+            <Backdrop onClick={this.handleBackdropClick}>
+                <ModalWindow>{this.props.children}</ModalWindow>
+            </Backdrop>
+        );
+    }
 }
